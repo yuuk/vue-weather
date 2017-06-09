452cbb3da3c18cefde2c86be21f4f796c2fe734f
凯余 <kaiyu@kaiyudeMacBook-Pro.local>
2017-06-09 22:38:44 +0800

    细节优化

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,7 +1,7 @@
 <template>
-    <section class="weather-page pr" v-if="weatherData!==null" :style="[wallPaperObj]">
+    <section class="weather-page pr" v-if="weatherData!==null && nowWeatherData!==null" :style="[wallPaperObj]">
         <header class="weather-page-header pr">
-            <p class="city">{{weatherData.location.name}}</p>
+            <p class="city">{{nowWeatherData.location.name}}</p>
             <p class="time">{{updateTime}} 发布</p>
             <span class="close pa">
                 <router-link :to="{name: 'list'}"><img src="../assets/images/close.svg"></router-link>
@@ -9,30 +9,30 @@
         </header>
 
         <div class="weather-page-info">
-            <p class="icon"><img :src="getIcon(weatherData.daily[0].text_day)"></p>
-            <p class="temperature"><span class="number">{{weatherData.daily[0].high}}</span> <sup class="degree-icon pr">°</sup></p>
-            <p class="status">{{weatherData.daily[0].text_day}}</p>
+            <p class="icon"><img :data-code="nowWeatherData.now.code" :src="getIcon(nowWeatherData.now.code)"></p>
+            <p class="temperature"><span class="number">{{nowWeatherData.now.temperature}}</span> <sup class="degree-icon pr">°</sup></p>
+            <p class="status">{{nowWeatherData.now.text}}</p>
         </div>
 
         <footer class="weather-page-footer pa">
-            <ul class="other-info clearfix" v-if="nowWeatherData!==null">
+            <ul class="other-info clearfix">
                 <li>
                     <p class="key">{{getWind}}</p>
-                    <p class="value">{{nowWeatherData.wind_scale}}级</p>
+                    <p class="value">{{nowWeatherData.now.wind_scale}}级</p>
                 </li>
                 <li>
                     <p class="key">能见度</p>
-                    <p class="value">{{nowWeatherData.visibility ? nowWeatherData.visibility : '--'}}</p>
+                    <p class="value">{{nowWeatherData.now.visibility ? nowWeatherData.now.visibility : '--'}}</p>
                 </li>
                 <li>
                     <p class="key">相对湿度</p>
-                    <p class="value">{{nowWeatherData.humidity}}%</p>
+                    <p class="value">{{nowWeatherData.now.humidity}}%</p>
                 </li>
             </ul>
             <ul class="future-weather clearfix">
                 <li v-for="weather in weatherData.daily">
                     <p class="date">{{weather.date}}</p>
-                    <p class="icon"><img :src="getIcon(weather.text_day)"></p>
+                    <p class="icon"><img :src="getIcon(weather.code_day)"></p>
                     <p class="text">{{weather.text_day}}</p>
                     <p class="temperature">{{weather.low}}° / {{weather.high}}°</p>
                 </li>
@@ -66,7 +66,7 @@ export default {
     computed: {
         // 计算天气更新时间
         updateTime () {
-            let time = new Date(this.weatherData.last_update);
+            let time = new Date(this.nowWeatherData.last_update);
             let fix = function (num) {
                 return num-0 <= 0 ? '0'+num : num;
             }
@@ -74,7 +74,7 @@ export default {
         },
         // 计算风向
         getWind () {
-            let weather = this.nowWeatherData;
+            let weather = this.nowWeatherData.now;
             if (weather !== null) {
                 return weather.wind_direction !== '无持续风向' ? `${weather.wind_direction}风` : `${weather.wind_direction}`;
             }
@@ -135,16 +135,16 @@ export default {
                 language: 'zh-Hans',
                 unit: 'c'
             }).then(json => {
-                this.nowWeatherData = json.results[0].now;
+                this.nowWeatherData = json.results[0];
             }).catch(err => {
                 console.error(err.statusText);
             });
         },
         // 匹配天气图标
-        getIcon (text) {
+        getIcon (code) {
             let src = icon[icon.length-1].src
             icon.every((o, i, arr) => {
-                if(o.cn === text) {
+                if(o.code === code-0) {
                     src = o.src;
                     return false;
                 }
